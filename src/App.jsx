037c680a9b005b473f02a1c45bb6423af3a26161
Adam H <ad.hauck@gmail.com>
2025-03-11 17:25:31 -0700

    add routing and navigation components with error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import WorkPage from './pages/WorkPage';
+import SinglePage from './pages/SinglePage';
+import ContactPage from './pages/ContactPage';
+import ErrorPage from './pages/ErrorPage';
+import Navigation from './components/Navigation';
 
 function App() {
 
   return (
-    <>
-    <HomePage />
-    </>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/work" element={<WorkPage />} />
+        <Route path="/work/:id" element={<SinglePage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path='*' element={<ErrorPage />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WorkPage = () => {
+    return (
+        <div>
+            <h1>Error Page</h1>
+            <p>This page is the 404 error page</p>
+        </div>
+    );
+    };
+
+export default WorkPage;
