831e9f0a13e944d6df03c59f06be44d81c29a6b5
Adam H <ad.hauck@gmail.com>
2025-03-12 00:21:58 -0700

    fetch work data from API and display on WorkPage

--- src/pages/WorkPage.jsx
+++ src/pages/WorkPage.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
 // eslint-disable-next-line
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { REST_PATH } from '../globals/globals';
+import Loading from '../components/Loading';
 
 const WorkPage = () => {
+    const workRestPath = `${REST_PATH}ahdesigns-work?acf_format=standard`;
+
+    const [workData, setWorkData] = useState([]);
+    const [workLoaded, setWorkLoaded] = useState(false);
+
+    useEffect(() => {
+        const fetchWorkData = async () => {
+        const workResponse = await fetch(workRestPath);
+        if (!workResponse.ok) {
+            setWorkLoaded(false);
+            console.error('There was a problem fetching the work data');
+            return;
+        } else {
+            const workFetched = await workResponse.json();
+            setWorkData(workFetched);
+            setWorkLoaded(true);    
+        }
+    }
+    fetchWorkData();
+    }
+    , [workRestPath]);
+
+
     return (
         <motion.div
             initial={{ opacity: 0 }}
@@ -14,6 +41,31 @@ const WorkPage = () => {
             <h1>Work Page</h1>
             <p>This page shows all the projects</p>
         </div>
+        
+        <section>
+            {workLoaded ?
+            
+            <div> 
+            {workData.map((project, i) => {
+                // Check if the project should have the 'featured-project' class
+                const projectClass = project["featured-work"][0] === 2 ? 'project featured-project' : 'project';
+
+                return (
+                <div className={projectClass} key={i}>
+                    <Link to={`/work/${project.slug}`} >
+                    {project.featured_images['medium'] && (
+                        <img srcSet={project.featured_images['medium'].srcset} />
+                    )}
+                    <h2>{project.title.rendered}</h2>
+                    </Link>
+                </div>
+                );
+            })}
+            </div>
+        :  
+            <Loading />
+        }
+        </section>
         </motion.div>
     );
     };
