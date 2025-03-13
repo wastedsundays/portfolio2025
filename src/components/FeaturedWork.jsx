b0ff252e6173259e490ed9ed5a998470a82218d2
Adam H <ad.hauck@gmail.com>
2025-03-13 00:25:01 -0700

    update featured work component with 4 random projects

--- src/components/FeaturedWork.jsx
+++ src/components/FeaturedWork.jsx
@@ -1,12 +1,64 @@
+import { useEffect, useState } from "react";
+import { REST_PATH } from "../globals/globals";
+import { Link } from "react-router-dom";
+import Loading from "./Loading";
+
 const FeaturedWork = () => {
 
+    const featuredWorkRestPath = `${REST_PATH}ahdesigns-work?acf_format=standard&featured-work=2`;
+    const [featuredWorkData, setFeaturedWorkData] = useState ([]);
+    const [featuredWorkLoaded, setFeaturedWorkLoaded] = useState (false);
+
+    useEffect(() => {
+        const fetchFeaturedWorkData = async () => {
+            const response = await fetch(featuredWorkRestPath);
+            if (!response.ok) {
+                setFeaturedWorkLoaded(false);
+                console.log('There was a problem fetching the data');
+                return;
+            } else{
+                const featuredWorkFetched = await response.json();
+                setFeaturedWorkData(featuredWorkFetched);
+                setFeaturedWorkLoaded(true); 
+            }
+        }
+        fetchFeaturedWorkData();
+    }, [featuredWorkRestPath]);
+
+    const shuffleArray = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    const randomData = (data) => {
+        const shuffledData = shuffleArray([...data]);
+        const displayData = shuffledData.slice(0, 4);
+        return displayData;
+    }
+
+    const featuredItems = randomData(featuredWorkData);
+
     return (
-        <div>
-            <article><img src="https://picsum.photos/300" alt="placeholder" /></article>
-            <article><img src="https://picsum.photos/300" alt="placeholder" /></article>
-            <article><img src="https://picsum.photos/300" alt="placeholder" /></article>
-            <article><img src="https://picsum.photos/300" alt="placeholder" /></article>
-        </div>
+        <>
+        {featuredWorkLoaded ? ( 
+            <div>
+                {featuredItems.map((project, i) => (
+                    <div key={i}>
+                        {project.title.rendered}
+                        <Link to={`/work/${project.slug}`}>
+                            Details
+                        </Link>
+                    </div>
+                ))}
+            </div>
+        ) : (
+            <Loading />
+        )}
+
+        </>
 
     )};
 
