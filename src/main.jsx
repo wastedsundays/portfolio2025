4dc379850865198faebe7f0389beddc040b744ae
Adam H <ad.hauck@gmail.com>
2025-03-20 00:56:39 -0700

    Set up sass for styling - tempo font and color

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './scss/styles.scss'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
