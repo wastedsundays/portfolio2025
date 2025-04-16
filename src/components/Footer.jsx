40b9bafb3650eba79e9eb54470da87eaf56ee918
Adam H <ad.hauck@gmail.com>
2025-04-16 02:36:18 -0700

    Added email context to footer & icon display

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,26 @@
+import EmailContext from '../context/EmailContext';
+import { useContext } from 'react';
+import IconLinkedin from '../assets/IconLinkedin';
+import IconGit from '../assets/IconGit';
+
+
+
+
 const Footer = () => {
+    const { emailSent } = useContext(EmailContext)
+
     const year = new Date().getFullYear();
 
     return (
         <footer>
             <p>&copy; {year} Adam H.</p>
+            {!emailSent && (
+
+                <div className='footer-icons'>                
+                <IconLinkedin />
+                <IconGit />
+                </div>
+                )}
 
 
         </footer>
